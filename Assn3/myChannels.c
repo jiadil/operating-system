12bda04ed7e2dee8060dffab18c11b432d04ea65
Nick <bz86nick@gmail.com>
2023-06-22 00:09:23 -0700

    added notes

--- Assn3/myChannels.c
+++ Assn3/myChannels.c
@@ -1,5 +1,18 @@
+/*  A multi-threaded program that takes 16-bit integer input 
+    from multiple channels and formulates a final input by
+    adding samples form all channels with optional low-pass
+    and/or amplification values.
+
+    IMPORTANT NOTE:
+    The alpha computation is first performed on the sample before amplification.
+    The final sample value should be rounded up to an integer value
+    If integer overflow, the final outut value is 65535 (max 16bit int value)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+
+
 
 int main(int argc, char *argv[]) {
     
